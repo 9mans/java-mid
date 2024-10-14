fb94541098fd2465475d4c3f74dc63c274e57b04
9mans <rbska1234@naver.com>
2024-10-14 13:54:45 +0900

    about object

---
+++ src/lang/object/ObjectMain.java
@@ -0,0 +1,16 @@
+package lang.object;
+
+public class ObjectMain {
+
+    // 오브젝트는 객체의 공통기능을 모아두기 위해 만든 클래스라고 생각하자 
+    public static void main(String[] args) {
+
+        Child child = new Child();
+        child.childMethod();
+        child.parentMethod();
+
+        // toString은 오브젝트 클래스의 메서드
+        String string = child.toString();
+        System.out.println(string);
+    }
+}
---
+++ src/lang/object/Parent.java
@@ -0,0 +1,9 @@
+package lang.object;
+
+// 클래스의 최상위 부모는 object 클래스
+public class Parent {
+
+    public void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+}
